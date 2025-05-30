d4719350b5342fa652dff9a296b12a248ca97d7e
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2025-05-30 13:46:08 -0300

    add startup
    se configura API para trabajar mediante startup

---
+++ Template-APIvs/Template-APIvs/Startup.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Template_APIvs
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            //app.UseCors();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            //app.UseAuthentication();
+
+            app.UseEndpoints(endpoints => endpoints.MapControllers());
+        }
+    }
+}
