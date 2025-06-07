a4039da49a68ed0199eebbd4dc63034767261c2d
escuelamanuelbelgrano <escuelamanuelbelgrano@unc.edu.ar>
2025-06-06 22:31:58 -0300

    Creacion de arquitectura
    se crean los proyectos para la arquitectura de la aplicacion y la estructura de cada capa de la misma

---
+++ Template-APIvs/Dominio/DomainServices/DummyDomainService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.DomainServices
+{
+    //<summary>
+    //clase que representa un servicio de dominio.
+    //manejan logica que no esresponsabilidad de las entidades
+    //</summary>
+    internal class DummyDomainService
+    {
+        
+    
+    }
+}
---
+++ Template-APIvs/Dominio/Others/Utils/Dummyutils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio.Others.Utils
+{
+    // <summary>
+    // clase que mantiene logica auxiliar (aislada) de la logica
+    // de negocio. puede contener funciones de todo tipo.
+    //</summary>
+    internal static class Dummyutils
+    {
+        public static void DummyMethod()
+        {
+
+        }
+        public static void DummExtensionMethod(this string arg)
+        {
+
+        }
+
+
+    }
+}
