c66250f517119edcfea3260aed76232e5ce8e309
jermiasoliva0805 <jeremiasoliva0805@mi.unc.edu.ar>
2025-06-12 11:26:27 -0300

    .

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public abstract class Animal
+    {
+        public abstract void EmitirSonido();
+    }
+
+    public class Vaca : Animal
+    {
+        public override void EmitirSonido()
+        {
+            Console.WriteLine("Muuu");
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            Animal a = new Vaca();
+            a.EmitirSonido();
+        }
+    }
+
+
+}
